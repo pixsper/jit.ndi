37692ff1d412e8d0f2ca7d9731cb30747cbc7429
pixsperdavid <david@pixsper.com>
2024-07-25 12:01:02 +0900

    Update version number

--- source/jit.ndi/version.h
+++ source/jit.ndi/version.h
@@ -3,7 +3,7 @@
 
 #define JIT_NDI_VERSION_MAJOR 0
 #define JIT_NDI_VERSION_MINOR 3
-#define JIT_NDI_VERSION_BUGFIX 1
+#define JIT_NDI_VERSION_BUGFIX 2
 
 #define JIT_NDI_COPYRIGHT "Copyright (C) 2024 Pixsper Ltd."
 
